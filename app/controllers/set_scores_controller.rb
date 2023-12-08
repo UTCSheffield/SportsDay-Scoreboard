66d94da6f558a4ca8f6bf8c2b157b9e3c0ebe65a
Mr Eggleton <meggleton@utcsheffield.org.uk>
2023-12-08 13:26:03 +0000

    Add filters for the sports (schonky code).

--- app/controllers/set_scores_controller.rb
+++ app/controllers/set_scores_controller.rb
@@ -1,7 +1,9 @@
 class SetScoresController < ApplicationController
   def index
-    if (params[:year].nil?)
+    if (params[:year].nil? and params[:activity].nil?)
       @events = Event.order("id ASC")
+    elsif (params[:year].nil? )
+      @events = Event.where(activity: params[:activity]).order("id ASC")
     else
       @events = Event.where(year: params[:year]).order("id ASC")
     end
