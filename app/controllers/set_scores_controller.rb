0adfc22fb3494058fa126057797f0cfe52ae28f3
northeastprince <matt7@hey.com>
2023-12-07 19:08:35 -0500

    🧹

--- app/controllers/set_scores_controller.rb
+++ app/controllers/set_scores_controller.rb
@@ -1,18 +1,16 @@
-require 'json'
 class SetScoresController < ApplicationController
-    skip_before_action :verify_authenticity_token if Rails.env.development?
-    def index
-        if (params[:year].nil?)
-            @events = Event.order("id ASC")
-        else
-            @events = Event.where(year: params[:year]).order("id ASC")
-        end
+  def index
+    if (params[:year].nil?)
+      @events = Event.order("id ASC")
+    else
+      @events = Event.where(year: params[:year]).order("id ASC")
     end
-    def update
-        event = Event.find_by(id: params[:id])
-        event.update(sharman: params[:sharman], winston: params[:winston], ennis: params[:ennis], turing: params[:turing])
-        event.save
-        ActionCable.server.broadcast("update_scores_channel", event.to_json)
-        redirect_to "/set_score"
-    end
+  end
+
+  def update
+    event = Event.find_by(id: params[:id])
+    event.update(sharman: params[:sharman], winston: params[:winston], ennis: params[:ennis], turing: params[:turing])
+    ActionCable.server.broadcast("update_scores_channel", event.to_json)
+    redirect_to "/set_score"
+  end
 end
