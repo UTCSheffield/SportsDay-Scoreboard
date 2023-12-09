351e0e1cf5c3d18d71b13a206c8c3d1aa161518e
matt7@hey.com <matt7@hey.com>
2023-12-09 08:16:58 -0500

    Use Turbo page refreshes for live updates

--- app/controllers/set_scores_controller.rb
+++ app/controllers/set_scores_controller.rb
@@ -12,12 +12,5 @@ class SetScoresController < ApplicationController
   def update
     event = Event.find_by(id: params[:id])
     event.update(sharman: params[:sharman], winston: params[:winston], ennis: params[:ennis], turing: params[:turing])
-    ActionCable.server.broadcast("update_scores_channel", {
-      year: event.year,
-      sharman: Event.where(year: event.year).sum(:sharman),
-      turing: Event.where(year: event.year).sum(:turing),
-      winston: Event.where(year: event.year).sum(:winston),
-      ennis: Event.where(year: event.year).sum(:ennis)
-    })
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,3 @@
 class Event < ApplicationRecord
+  broadcasts_refreshes
 end
