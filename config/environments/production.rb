7b44d4b4c048d7c3737f7a016314b0fd028d9f7c
Sean Outram <outramsean0@gmail.com>
2023-12-12 17:42:02 +0000

    Swapout allowed hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  config.hosts << "sutc-sportsday-scoreboard.fly.dev"
+  config.hosts << "sportsday-scoreboard-production.up.railway.app"
   config.serve_static_files = true
   
 end
