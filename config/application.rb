b31be673caa2d0016486071c4abeacf924697006
Sean Outram <outramsean0@gmail.com>
2023-12-12 16:48:22 +0000

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,6 @@ module SutcSportsDayScoreboard
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.web_console.development_only = false
   end
 end
