f8aedd94fddd93a4c1b0a36e9c43d2e34a1a462a
Sean Outram <outramsean0@gmail.com>
2023-12-13 11:41:08 +0000

    Add support for papertrail whodunnit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   include ActionController::Cookies
+  before_action :set_paper_trail_whodunnit
+
+  def current_user
+    session[:grant]["user"]["email"]
+  end
 end
