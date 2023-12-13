d60c2e01367d4e183297cc166743c5e76c08ea24
Sean Outram <outramsean0@gmail.com>
2023-12-13 11:09:59 +0000

    Gib console back

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,6 @@ module SutcSportsDayScoreboard
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.web_console.development_only = false
+    config.web_console.permissions = '0.0.0.0/0'
   end
 end
