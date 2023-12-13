05210b9b17aa251f3ba22e5e4b2caa39928def51
Sean Outram <outramsean0@gmail.com>
2023-12-13 12:57:53 +0000

    REVERT

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,3 @@
 class ApplicationController < ActionController::Base
   include ActionController::Cookies
-  before_action :set_paper_trail_whodunnit
-
-  def current_user
-    session[:grant]["user"]["email"]
-  end
 end
