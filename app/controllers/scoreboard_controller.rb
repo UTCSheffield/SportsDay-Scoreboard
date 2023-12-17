7ada9890fb922b104e46abfb8b9c4be234102ade
Sean Outram <outramsean0@gmail.com>
2023-12-17 14:58:41 +0000

    Those additional statistics

--- app/controllers/scoreboard_controller.rb
+++ app/controllers/scoreboard_controller.rb
@@ -1,3 +1,12 @@
 class ScoreboardController < ApplicationController
-    def index; end
+    def index
+        year_totals = Event.select(:year, 'SUM(turing) as turing', 'SUM(winston) as winston', 'SUM(sharman) as sharman', 'SUM(ennis) as ennis').group(:year)
+        @leading_year = year_totals.max_by { |result| result.turing + result.winston + result.sharman + result.ennis }.year
+
+        column_with_highest_value = [:turing, :winston, :sharman, :ennis].max_by do |column|
+            year_totals.max_by { |result| result.send(column) }.send(column)
+        end
+
+        @leading_form = column_with_highest_value.capitalize
+    end
 end
