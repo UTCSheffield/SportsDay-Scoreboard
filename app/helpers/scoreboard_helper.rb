5efdd6d7481b34feb49e37896b266fb02cec328b
Sean Outram <outramsean0@gmail.com>
2023-12-20 14:05:19 +0000

    Traditional Ordering of Houses #10

--- app/helpers/scoreboard_helper.rb
+++ app/helpers/scoreboard_helper.rb
@@ -7,7 +7,7 @@ module ScoreboardHelper
     sharman = content_tag(:td, "#{events.sum(:sharman)}", id: "#{year}-sharman", class: "sharman")
     ennis = content_tag(:td, "#{events.sum(:ennis)}", id: "#{year}-ennis", class: "ennis")
     content_tag(:tr,
-      [header, turing, winston, sharman, ennis].join.html_safe
+      [header, winston, ennis, sharman, turing].join.html_safe
     )
   end
 end
