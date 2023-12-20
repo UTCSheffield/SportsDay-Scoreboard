b06045b13be35933f7de2bf2c593c772d251dd9f
Sean Outram <outramsean0@gmail.com>
2023-12-20 13:49:55 +0000

    Update tables to include form colours

--- app/helpers/scoreboard_helper.rb
+++ app/helpers/scoreboard_helper.rb
@@ -2,10 +2,10 @@ module ScoreboardHelper
   def render_scoreboard_rows(year)
     events = Event.all.where(year: year)
     header = content_tag(:th, "Year #{year}")
-    turing = content_tag(:td, "#{events.sum(:turing)}", id: "#{year}-turing")
-    winston = content_tag(:td, "#{events.sum(:winston)}", id: "#{year}-winston")
-    sharman = content_tag(:td, "#{events.sum(:sharman)}", id: "#{year}-sharman")
-    ennis = content_tag(:td, "#{events.sum(:ennis)}", id: "#{year}-ennis")
+    turing = content_tag(:td, "#{events.sum(:turing)}", id: "#{year}-turing", class: "turing")
+    winston = content_tag(:td, "#{events.sum(:winston)}", id: "#{year}-winston", class: "winston")
+    sharman = content_tag(:td, "#{events.sum(:sharman)}", id: "#{year}-sharman", class: "sharman")
+    ennis = content_tag(:td, "#{events.sum(:ennis)}", id: "#{year}-ennis", class: "ennis")
     content_tag(:tr,
       [header, turing, winston, sharman, ennis].join.html_safe
     )
