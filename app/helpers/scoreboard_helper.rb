14c14895ec64df35c08ef5a8dce8e5eab5550e5b
Sean Outram <outramsean0@gmail.com>
2023-12-21 09:12:54 +0000

    Totals on the scoreboard

--- app/helpers/scoreboard_helper.rb
+++ app/helpers/scoreboard_helper.rb
@@ -6,6 +6,18 @@ module ScoreboardHelper
     winston = content_tag(:td, "#{events.sum(:winston)}", id: "#{year}-winston", class: "winston")
     sharman = content_tag(:td, "#{events.sum(:sharman)}", id: "#{year}-sharman", class: "sharman")
     ennis = content_tag(:td, "#{events.sum(:ennis)}", id: "#{year}-ennis", class: "ennis")
+    total = content_tag(:td, "#{events.sum(:turing) + events.sum(:winston) + events.sum(:sharman) + events.sum(:ennis)}")
+    content_tag(:tr,
+      [header, winston, ennis, sharman, turing, total].join.html_safe
+    )
+  end
+  def render_column_totals
+    events = Event.all
+    header = content_tag(:th, "Totals")
+    turing = content_tag(:td, "#{events.sum(:turing)}", class: "turing")
+    winston = content_tag(:td, "#{events.sum(:winston)}", class: "winston")
+    sharman = content_tag(:td, "#{events.sum(:sharman)}", class: "sharman")
+    ennis = content_tag(:td, "#{events.sum(:ennis)}", class: "ennis")
     content_tag(:tr,
       [header, winston, ennis, sharman, turing].join.html_safe
     )
