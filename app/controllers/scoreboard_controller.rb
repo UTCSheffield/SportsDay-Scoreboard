76637e8c9352085346426447f3b2048d79c3a83a
Sean Outram <outramsean0@gmail.com>
2024-05-29 13:43:05 +0100

    Update scoreboard_controller.rb

--- app/controllers/scoreboard_controller.rb
+++ app/controllers/scoreboard_controller.rb
@@ -1,4 +1,8 @@
 class ScoreboardController < ApplicationController
+    before_action :allow_iframe_requests
+    def allow_iframe_requests
+      response.headers.delete('X-Frame-Options')
+    end
     def index
         year_totals = Event.select(:year, 'SUM(turing) as turing', 'SUM(winston) as winston', 'SUM(sharman) as sharman', 'SUM(ennis) as ennis').group(:year)
         @leading_year = year_totals.max_by { |result| result.turing + result.winston + result.sharman + result.ennis }.year
